2543fd545b41a968d9b353d9efeb0df7ed879982
wangpf <oncwnuNiTDOl00Q4ak5mLqaYLgaU@git.weixin.qq.com>
2021-03-08 18:21:12 +0800

    map tmp

--- Map/dictionary.go
+++ Map/dictionary.go
@@ -2,7 +2,10 @@ package Map
 
 import "errors"
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 type Dictionary map[string]string
 
@@ -14,4 +17,19 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return name, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
