5139ef9ddec1dc7290f46bf6c52d39400dd933af
feiai2017 <274878379@qq.com>
2021-03-23 23:13:07 +0800

    http server tmp

---
+++ http-server/main.go
@@ -0,0 +1,13 @@
+package http_server
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
