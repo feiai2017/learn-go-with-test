d1dfde85ade27aa6a6c0eb4fe584d33ffa5cb5e4
feiai2017 <274878379@qq.com>
2021-03-24 08:32:20 +0800

    http server tmp

--- http-server/server.go
+++ http-server/server.go
@@ -14,20 +14,11 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-func (p *PlayerServer) ServerHTTP (w http.ResponseWriter, r *http.Request) {
+func (p *PlayerServer) ServeHTTP (w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
-	fmt.Fprint(w, GetPlayerScore(player))
+	score := p.store.GetPlayerScore(player)
+
+	fmt.Fprint(w, score)
 }
 
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
-	}
-
-	if name == "Floyd" {
-		return "10"
-	}
-
-	return ""
-}
