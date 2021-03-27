d5b14028b67a227567210b8249b55f7fc8e36f1c
feiai2017 <274878379@qq.com>
2021-03-27 16:14:30 +0800

    io

--- http_server/file_system_store.go
+++ http_server/file_system_store.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 )
 
 type FileSystemPlayerStore struct {
@@ -12,17 +13,11 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
 
-	info, err := file.Stat()
+	err := initialisePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte(""))
-		file.Seek(0, 0)
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -32,12 +27,32 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	}
 
 	return &FileSystemPlayerStore{
-		Database: json.NewEncoder(&tape{file: file}),
+		Database: json.NewEncoder(&tape{file}),
 		League:   league,
 	}, nil
 }
 
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
+}
+
 func (f *FileSystemPlayerStore) GetLeague() League {
+	sort.Slice(f.League, func(i, j int) bool {
+		return f.League[i].Wins > f.League[j].Wins
+	})
 
 	return f.League
 }
